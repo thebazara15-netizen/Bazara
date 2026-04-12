50907a02e17a2085770a2f6b6fbd6671f90ea4c5
Dhirendra-prasad <i.mdhirendra22@email.com>
2026-04-13 00:37:55 +0530

    fix DB error

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,14 +1,15 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'postgres',
-    logging: false
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-);
+});
 
 module.exports = sequelize;
