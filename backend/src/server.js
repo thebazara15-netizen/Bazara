4ee8b76077b386be8d86e729a4dd664f2d646bc3
Dhirendra-prasad <i.mdhirendra22@email.com>
2026-04-14 00:44:39 +0530

    added cart

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,3 @@
-// src/server.js
-
 require('dotenv').config();
 
 const app = require('./app');
@@ -9,21 +7,19 @@ const PORT = process.env.PORT || 5000;
 
 async function startServer() {
   try {
-    // Step 1: Connect DB
     await sequelize.authenticate();
     console.log('PostgreSQL Connected');
 
-    // Step 2: Sync tables
-    await sequelize.sync({ alter: true });
+    await sequelize.sync();
     console.log('Tables synced');
 
-    // Step 3: Start server
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
 
   } catch (error) {
     console.error('Error starting server:', error);
+    process.exit(1);
   }
 }
 
