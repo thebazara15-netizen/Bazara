89d3926ffdce7bb8f35dffe25e9063125fee932a
Dhirendra-prasad <i.mdhirendra22@email.com>
2026-04-13 11:59:55 +0530

    fix deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://bazara-pi.vercel.app/"
+    "https://bazara-pi.vercel.app"
   ],
   credentials: true
 }));
